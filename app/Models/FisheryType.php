839904b5549d353a0a2c9d9963c2f83edb34af3b
Amadeusz Lisiecki <amadeusz.lisiecki@onet.pl>
2025-08-14 15:21:43 +0200

    Dodanie typów łowisk.

---
+++ app/Models/FisheryType.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\LogOptions;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class FisheryType extends Model
+{
+    use LogsActivity, SoftDeletes;
+
+    protected $fillable = ['name'];
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()->logOnly($this->fillable);
+    }
+}
