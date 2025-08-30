ab3592044b8d2c2d87a7b638245fe0677a64eaf8
Amadeusz Lisiecki <amadeusz.lisiecki@onet.pl>
2025-08-30 15:33:15 +0200

    Dodanie ekranu z informacją o konieczności weryfikacji firmy.

--- app/Filament/Owner/Pages/VerifyCompany.php
+++ app/Filament/Owner/Pages/VerifyCompany.php
@@ -8,6 +8,11 @@ class VerifyCompany extends Page
 {
     protected static string $view = 'filament.owner.pages.verify-company';
 
+    public function getTitle(): string
+    {
+        return __('Create fishery wizard');
+    }
+
     public static function shouldRegisterNavigation(): bool
     {
         return false;
--- resources/views/filament/owner/pages/verify-company.blade.php
+++ resources/views/filament/owner/pages/verify-company.blade.php
@@ -1,3 +1,28 @@
 <x-filament-panels::page>
+    <div class="mb-2"><strong>{{ __('Step') . ' 2 / 3' }}</strong></div>
 
+    <p>{{ __('Verification transfer') }}</p>
+    <p>{{ __('Transfer for 1 złoty is required to verify company.') }}</p>
+
+    <div>
+        <x-filament::button
+            tag="a"
+            href="{{ route('filament.owner.resources.fisheries.create', [
+                'company' => request()->query('company'),
+                'wizard' => 1,
+            ]) }}"
+            color="success"
+        >
+            {{ __('Next') }}
+        </x-filament::button>
+        <x-filament::button
+            tag="a"
+            href="{{ route('filament.owner.resources.companies.create', [
+                'wizard' => 1,
+            ]) }}"
+            color="danger"
+        >
+            {{ __('Previous') }}
+        </x-filament::button>
+    </div>
 </x-filament-panels::page>
