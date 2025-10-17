77d3d20b5004345b3b34e76745173eea47040ad8
Amadeusz Lisiecki <amadeusz.lisiecki@onet.pl>
2025-10-17 11:36:45 +0200

    Dodanie uprawnień do zarządzania łowiskiem.

--- app/Policies/PositionPolicy.php
+++ app/Policies/PositionPolicy.php
@@ -3,10 +3,10 @@
 namespace App\Policies;
 
 use App\Models\User;
-use Spatie\Permission\Models\Role;
+use App\Models\Position;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
-class RolePolicy
+class PositionPolicy
 {
     use HandlesAuthorization;
 
@@ -15,15 +15,16 @@ class RolePolicy
      */
     public function viewAny(User $user): bool
     {
-        return $user->can('view_any_role');
+        return $user->can('view_any_position') || $user->hasRole('owner');
     }
 
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, Role $role): bool
+    public function view(User $user, Position $position): bool
     {
-        return $user->can('view_role');
+        return $user->can('view_position') || 
+               ($user->hasRole('owner') && $position->fishery->user_id === $user->id);
     }
 
     /**
@@ -31,23 +32,25 @@ class RolePolicy
      */
     public function create(User $user): bool
     {
-        return $user->can('create_role');
+        return $user->can('create_position') || $user->hasRole('owner');
     }
 
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, Role $role): bool
+    public function update(User $user, Position $position): bool
     {
-        return $user->can('update_role');
+        return $user->can('update_position') || 
+               ($user->hasRole('owner') && $position->fishery->user_id === $user->id);
     }
 
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, Role $role): bool
+    public function delete(User $user, Position $position): bool
     {
-        return $user->can('delete_role');
+        return $user->can('delete_position') || 
+               ($user->hasRole('owner') && $position->fishery->user_id === $user->id);
     }
 
     /**
@@ -55,15 +58,15 @@ class RolePolicy
      */
     public function deleteAny(User $user): bool
     {
-        return $user->can('delete_any_role');
+        return $user->can('delete_any_position');
     }
 
     /**
      * Determine whether the user can permanently delete.
      */
-    public function forceDelete(User $user, Role $role): bool
+    public function forceDelete(User $user, Position $position): bool
     {
-        return $user->can('{{ ForceDelete }}');
+        return $user->can('force_delete_position');
     }
 
     /**
@@ -71,15 +74,15 @@ class RolePolicy
      */
     public function forceDeleteAny(User $user): bool
     {
-        return $user->can('{{ ForceDeleteAny }}');
+        return $user->can('force_delete_any_position');
     }
 
     /**
      * Determine whether the user can restore.
      */
-    public function restore(User $user, Role $role): bool
+    public function restore(User $user, Position $position): bool
     {
-        return $user->can('{{ Restore }}');
+        return $user->can('restore_position');
     }
 
     /**
@@ -87,15 +90,15 @@ class RolePolicy
      */
     public function restoreAny(User $user): bool
     {
-        return $user->can('{{ RestoreAny }}');
+        return $user->can('restore_any_position');
     }
 
     /**
      * Determine whether the user can replicate.
      */
-    public function replicate(User $user, Role $role): bool
+    public function replicate(User $user, Position $position): bool
     {
-        return $user->can('{{ Replicate }}');
+        return $user->can('replicate_position');
     }
 
     /**
@@ -103,6 +106,6 @@ class RolePolicy
      */
     public function reorder(User $user): bool
     {
-        return $user->can('{{ Reorder }}');
+        return $user->can('reorder_position');
     }
 }
